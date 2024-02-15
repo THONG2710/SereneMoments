d230e4016afc79dac50bec91e2883bc7ea5279a4
Nguyen Van Thong <5506nguyenthong@gmail.com>
2024-02-15 07:35:11 +0700

    commit

--- src/Navigation/AuthenticationNavigation.tsx
+++ src/Navigation/AuthenticationNavigation.tsx
@@ -1,11 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Login from '../Screen/Authentication/Login/Login';
+
+const tab = createBottomTabNavigator();
 
 const AuthenticationNavigation = () => {
   return (
-    <View>
-      <Text>AuthenticationNavigation</Text>
-    </View>
+    <NavigationContainer>
+      <tab.Navigator>
+        <tab.Screen name='LoginScreen' component={Login}/>
+      </tab.Navigator>
+    </NavigationContainer>
   )
 }
 
