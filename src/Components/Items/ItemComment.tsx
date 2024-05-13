af3391a094288cd18ee7297be75627cafbd05ba5
huyho <huyho17032003@gmail.com>
2024-05-13 20:55:20 +0700

    huyne

--- src/Components/Items/ItemComment.tsx
+++ src/Components/Items/ItemComment.tsx
@@ -5,36 +5,72 @@ import {
   Text,
   View,
   ViewProps,
+  TouchableOpacity
 } from 'react-native';
-import React from 'react';
-import {ResponseCommentModel} from '../../Models/Model';
-import {onConvertEpochtime} from '../../Service/Service';
-import {Colors} from '../../Resource/colors';
+import React, { useState } from 'react';
+import { ResponseCommentModel } from '../../Models/Model';
+import { onConvertEpochtime } from '../../Service/Service';
+import { Colors } from '../../Resource/colors';
+import Modal from 'react-native-modal/dist/modal';
+import { useAppSelector } from '../../Redux/Hook';
 
-interface ItemCommentProps{
+
+interface ItemCommentProps {
   comment: ResponseCommentModel;
 }
 
 const ItemComment: React.FC<ItemCommentProps> = props => {
-  const {comment} = props;
+  const { comment } = props;
+  const [isVisibleMenu, setisVisibleMenu] = useState(false)
+  const user = useAppSelector(state => state.Authentication.myAccount)
+  const testCmt = () =>{
+        if(comment.userid===user._id){
+          setisVisibleMenu(true)
+        }else{
+          setisVisibleMenu(false)
+        }
+        
+  }
+
   return (
-    <View style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={testCmt}>
       <Image
         style={styles.imgAvatar}
         source={
           comment.avatar
-            ? {uri: comment.avatar}
+            ? { uri: comment.avatar }
             : require('../../Resource/images/avatar.png')
         }
       />
+
       <View style={styles.smallContainer}>
         <Text style={styles.txtName}>{comment.username}</Text>
+
         <Text style={styles.txtContent}>{comment.content}</Text>
+
         <Text style={styles.txtDate}>
           {onConvertEpochtime(Number(comment.createdat))}
         </Text>
+
       </View>
-    </View>
+      <Modal
+        animationIn={'fadeIn'}
+        animationOut={'fadeOutDown'}
+        onBackdropPress={() => setisVisibleMenu(false)}
+        isVisible={isVisibleMenu}
+        children={
+          <View style={styles.dialogMenu}>
+            <TouchableOpacity style={styles.btn} >
+              <Text style={[styles.txt]}>Xóa</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.btn}>
+              <Text style={styles.txt}>Chỉnh sửa</Text>
+            </TouchableOpacity>
+          </View>
+        }
+      />
+
+    </TouchableOpacity>
   );
 };
 
@@ -74,4 +110,23 @@ const styles = StyleSheet.create({
   txtDate: {
     fontSize: 12,
   },
+  dialogMenu: {
+    backgroundColor: Colors.WHITE,
+    width: '100%',
+    height: Dimensions.get('screen').height / 10,
+    marginTop: Dimensions.get('screen').height/4*3 ,
+    
+    borderRadius: 10,
+  },
+
+  btn: {
+    alignItems: 'center',
+    height: 35,
+    justifyContent: 'center',
+  },
+
+  txt: {
+    color: Colors.BLACK,
+  },
+
 });
