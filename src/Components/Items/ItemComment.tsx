f3ad92ebe017c07f5e5e5edffc434432e34f14a2
huyho <huyho17032003@gmail.com>
2024-05-13 22:06:32 +0700

    huy

--- src/Components/Items/ItemComment.tsx
+++ src/Components/Items/ItemComment.tsx
@@ -5,7 +5,9 @@ import {
   Text,
   View,
   ViewProps,
-  TouchableOpacity
+  TouchableOpacity,
+  Alert,
+  TextInput
 } from 'react-native';
 import React, { useState } from 'react';
 import { ResponseCommentModel } from '../../Models/Model';
@@ -13,6 +15,7 @@ import { onConvertEpochtime } from '../../Service/Service';
 import { Colors } from '../../Resource/colors';
 import Modal from 'react-native-modal/dist/modal';
 import { useAppSelector } from '../../Redux/Hook';
+import { ID_ADRESS, getData, postData } from '../../Service/RequestMethod';
 
 
 interface ItemCommentProps {
@@ -22,14 +25,79 @@ interface ItemCommentProps {
 const ItemComment: React.FC<ItemCommentProps> = props => {
   const { comment } = props;
   const [isVisibleMenu, setisVisibleMenu] = useState(false)
+  const [testEdit, settestEdit] = useState(false)
+  const [newContent, setnewContent] = useState("")
+
+
   const user = useAppSelector(state => state.Authentication.myAccount)
-  const testCmt = () =>{
-        if(comment.userid===user._id){
-          setisVisibleMenu(true)
-        }else{
-          setisVisibleMenu(false)
+  const testCmt = () => {
+    if (comment.userid === user._id) {
+      setisVisibleMenu(true)
+    } else {
+      setisVisibleMenu(false)
+    }
+
+  }
+  const onConfirmDelete = () => {
+    setisVisibleMenu(false);
+    Alert.alert('Thông báo', 'Bạn muốn xóa khoảnh khắc này?', [
+      {
+        onPress: () => onDelete(),
+        text: 'Xóa',
+      },
+      {
+        text: 'Hủy',
+        style: 'cancel',
+      },
+    ]);
+  };
+
+  const onDelete = async () => {
+    try {
+      const res = await getData(
+        'http://' + ID_ADRESS + ':3000/api/comments/deleteComment/' + comment._id,
+      );
+      console.log(comment._id);
+
+      if (res.result) {
+        Alert.alert('Thông báo', 'Xóa thành công', [
+          {
+
+            text: 'OK',
+          },
+        ]);
+      }
+    } catch (error) { }
+  };
+  const Edit = () => {
+    settestEdit(true);
+    setnewContent(comment.content.toString());
+    setisVisibleMenu(false)
+  }
+  const updateComment=async()=>{
+      if(newContent!=comment.content){
+        try {
+          const data = {
+            id: comment._id,
+            content:newContent
+          }
+          const res = await postData(
+            'http://' + ID_ADRESS + ':3000/api/comments/updateComment' , data
+          )
+          if(res.result){
+            settestEdit(false);
+            setnewContent(newContent)
+            Alert.alert('Thông báo', 'Cập nhật thành công', [
+              {
+    
+                text: 'OK',
+              },
+            ]);
+          }
+        } catch (error) {
+          
         }
-        
+      }
   }
 
   return (
@@ -45,14 +113,22 @@ const ItemComment: React.FC<ItemCommentProps> = props => {
 
       <View style={styles.smallContainer}>
         <Text style={styles.txtName}>{comment.username}</Text>
+        {
+          testEdit ? <TextInput value={newContent} onChangeText={value => setnewContent(value)}></TextInput> : <Text style={styles.txtContent}>{comment.content}</Text>
+        }
 
-        <Text style={styles.txtContent}>{comment.content}</Text>
 
         <Text style={styles.txtDate}>
           {onConvertEpochtime(Number(comment.createdat))}
         </Text>
-
+        {
+          testEdit ? <TouchableOpacity onPress={updateComment}>
+          <Text>Lưu</Text>
+        </TouchableOpacity>  : null
+        }
+       
       </View>
+
       <Modal
         animationIn={'fadeIn'}
         animationOut={'fadeOutDown'}
@@ -60,10 +136,10 @@ const ItemComment: React.FC<ItemCommentProps> = props => {
         isVisible={isVisibleMenu}
         children={
           <View style={styles.dialogMenu}>
-            <TouchableOpacity style={styles.btn} >
+            <TouchableOpacity style={styles.btn} onPress={onConfirmDelete} >
               <Text style={[styles.txt]}>Xóa</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.btn}>
+            <TouchableOpacity style={styles.btn} onPress={Edit}>
               <Text style={styles.txt}>Chỉnh sửa</Text>
             </TouchableOpacity>
           </View>
@@ -114,8 +190,8 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.WHITE,
     width: '100%',
     height: Dimensions.get('screen').height / 10,
-    marginTop: Dimensions.get('screen').height/4*3 ,
-    
+    marginTop: Dimensions.get('screen').height / 4 * 3,
+
     borderRadius: 10,
   },
 
